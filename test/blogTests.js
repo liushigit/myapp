56a307be7045d0890433ccc4826137599dcdd095
Shi Liu <liushie@gmail.com>
2014-04-26 16:10:22 +0800

    additions to tests
    moved tests to test
    added a test of POSTing to /blog/

--- test/blogTests.js
+++ test/blogTests.js
@@ -11,15 +11,39 @@ describe('GET /blog/', function() {
     });
 });
 
+describe('POST /blog/', function() {
+    it('post a new blog entry', function(done){
+    	request(app)
+			.post('/blog/')
+			.send({blog: {title:'Cool', body:'Cool Text'}})
+		    .expect(302)
+			.end(function(err) {
+				if (err) {
+					console.log(err);
+				}
+				done();
+			});
+    }); 
+});
+
 var entry;
 BlogEntry.find({}, function(err, docs) {
     entry = docs[1];
 });
 
 describe('GET /blog/', function() {
-    it('blog list', function(done) {
+	
+    it('blog entry edit form page', function(done) {
         request(app)
             .get('/blog/' + entry.id + '/edit')
-            .expect(200, done);
+            .expect(200)
+			.end(function(err, res) {
+				if (err) {
+					console.log(err);
+					throw err;
+				}
+				done();
+			});
     });
 });
+

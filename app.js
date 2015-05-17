1e90507a49b0ef2f65252270d8ee0b4e55bc0717
Shi Liu <liushie@gmail.com>
2015-05-17 21:16:22 +0800

    updated init  file and settings for production

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser(function (id, done) {
 });
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
